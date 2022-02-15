71be9e616033fb2b61bbb294b7d6f1c2b24c9c05
dboures <dboures@att.net>
2022-02-14 21:42:49 -0600

    test(wip): add mangoConfig and stub oracles

--- tests/mango-blender.ts
+++ tests/mango-blender.ts
@@ -1,18 +1,30 @@
 import * as anchor from "@project-serum/anchor";
 import { Program, Provider } from "@project-serum/anchor";
-import { Account, Keypair, PublicKey, SystemProgram, Transaction } from "@solana/web3.js";
+import {
+  Account,
+  Keypair,
+  PublicKey,
+  SystemProgram,
+  Transaction,
+} from "@solana/web3.js";
 import {
   SingleConnectionBroadcaster,
   SolanaProvider,
 } from "@saberhq/solana-contrib";
-import { Provider as SaberProvider } from "@saberhq/token-utils/node_modules/@saberhq/solana-contrib";
 import { NodeWallet } from "@project-serum/common";
 import { MangoBlender } from "../target/types/mango_blender";
-import { createMangoGroup, createQuoteToken, initializeProviderQuoteATA, MANGO_PROG_ID } from "./setupMango";
-import { MangoClient } from "@blockworks-foundation/mango-client";
+import {
+  createMangoGroup,
+  createToken,
+  initializeProviderATA,
+  initPriceOracles,
+  MANGO_PROG_ID,
+} from "./setupMango";
+import { Config, MangoClient } from "@blockworks-foundation/mango-client";
 import { Token, TOKEN_PROGRAM_ID } from "@solana/spl-token";
 import { getTokenAccount } from "@saberhq/token-utils";
 const assert = require("assert");
+
 
 describe("mango-blender", () => {
   let program: any;
@@ -27,7 +39,11 @@ describe("mango-blender", () => {
   let mangoAccountAddress: PublicKey;
   let mangoAccountBump: number;
 
+  let tokenA: Token;
+  let tokenB: Token;
   let quoteToken: Token;
+  let providerAATA: PublicKey;
+  let providerBATA: PublicKey;
   let providerQuoteATA: PublicKey;
   let mangoGroupPubkey: PublicKey;
   let client: MangoClient;
@@ -51,60 +67,115 @@ describe("mango-blender", () => {
       program.programId
     );
 
-    quoteToken =  await createQuoteToken(provider, payer);
-    mangoGroupPubkey = await createMangoGroup(provider, payer, quoteToken);
     client = new MangoClient(provider.connection, MANGO_PROG_ID);
 
-    [mangoAccountAddress, mangoAccountBump] = await PublicKey.findProgramAddress(
-      [mangoGroupPubkey.toBytes(), poolAddress.toBytes(), new anchor.BN(1).toArrayLike(Buffer, "le", 8)],
-      MANGO_PROG_ID
+    quoteToken = await createToken(provider, payer, 6);
+    tokenA = await createToken(provider, payer, 3);
+    tokenB = await createToken(provider, payer, 9);
+    mangoGroupPubkey = await createMangoGroup(provider, payer, quoteToken);
+
+    // add oracles
+    await initPriceOracles(payer, client, mangoGroupPubkey);
+
+    // set oracle prices
+    // await setOraclePrice('QUOTE', 1);
+    // await setOraclePrice('AAAA', 0.1);
+    // await setOraclePrice('BBBB', 2);
+
+    [mangoAccountAddress, mangoAccountBump] =
+      await PublicKey.findProgramAddress(
+        [
+          mangoGroupPubkey.toBytes(),
+          poolAddress.toBytes(),
+          new anchor.BN(1).toArrayLike(Buffer, "le", 8), // account_num
+        ],
+        MANGO_PROG_ID
+      );
+
+    // 5 tokens each
+    providerQuoteATA = await initializeProviderATA(
+      provider,
+      payer,
+      quoteToken,
+      5000000
     );
-
-    providerQuoteATA = await initializeProviderQuoteATA(provider, payer, quoteToken);
-
+    providerAATA = await initializeProviderATA(
+      provider,
+      payer,
+      tokenA,
+      5000
+    );
+    providerBATA = await initializeProviderATA(
+      provider,
+      payer,
+      tokenB,
+      5000000000
+    );
   });
 
   it("can create a liquidator pool, which includes a delegated mangoAccount for liqor", async () => {
-    const accountNum = new anchor.BN(1)
-    const tx = await program.rpc.createPool(poolNameBytes, poolBump, accountNum, {
-      accounts: {
-        pool: poolAddress,
-        admin: provider.wallet.publicKey,
-        mangoProgram: MANGO_PROG_ID,
-        mangoGroup: mangoGroupPubkey,
-        mangoAccount: mangoAccountAddress,
-        systemProgram: SystemProgram.programId,
-      },
-      signers: [payer],
-    });
+    const accountNum = new anchor.BN(1);
+    const tx = await program.rpc.createPool(
+      poolNameBytes,
+      poolBump,
+      accountNum,
+      {
+        accounts: {
+          pool: poolAddress,
+          admin: provider.wallet.publicKey,
+          mangoProgram: MANGO_PROG_ID,
+          mangoGroup: mangoGroupPubkey,
+          mangoAccount: mangoAccountAddress,
+          systemProgram: SystemProgram.programId,
+        },
+        signers: [payer],
+      }
+    );
 
     const initializedPool = await program.account.pool.fetch(poolAddress);
     assert.ok(
       initializedPool.admin.toBase58() === provider.wallet.publicKey.toBase58()
     );
 
-    const mangoAccount = await client.getMangoAccount(mangoAccountAddress, MANGO_PROG_ID);
+    const mangoAccount = await client.getMangoAccount(
+      mangoAccountAddress,
+      MANGO_PROG_ID
+    );
     assert.ok(mangoAccount.metaData.isInitialized);
-    assert.ok(mangoAccount.delegate.equals(provider.wallet.publicKey))
+    assert.ok(mangoAccount.delegate.equals(provider.wallet.publicKey));
   });
 
-  it("allows a user to deposit into the delegated mangoAccount", async () => {
-    const providerQuoteBefore = await getTokenAccount(provider as unknown as SaberProvider, providerQuoteATA);
+  it("allows a user to deposit QUOTE into the delegated mangoAccount", async () => {
+    const providerQuoteBefore = await getTokenAccount(
+      provider,
+      providerQuoteATA
+    );
     assert.ok(providerQuoteBefore.amount.eq(new anchor.BN(5000000)));
 
     const group = await client.getMangoGroup(mangoGroupPubkey);
     const rootBanks = await group.loadRootBanks(provider.connection);
-    const rootBankPubkeys = rootBanks.map(rb => rb?.publicKey).filter(pk => pk !== undefined) as PublicKey[]
+    const rootBankPubkeys = rootBanks
+      .map((rb) => rb?.publicKey)
+      .filter((pk) => pk !== undefined) as PublicKey[];
     const tokenIndex = group.getTokenIndex(quoteToken.publicKey);
-    const nodeBanks = await rootBanks[tokenIndex]?.loadNodeBanks(provider.connection);
+    const nodeBanks = await rootBanks[tokenIndex]?.loadNodeBanks(
+      provider.connection
+    );
     const mangoCache = await group.loadCache(provider.connection);
     if (!nodeBanks) {
       throw Error;
     }
 
-    await client.cacheRootBanks(mangoGroupPubkey, mangoCache.publicKey, rootBankPubkeys, payer as unknown as Account)
-    
-    const tx = await program.rpc.deposit(new anchor.BN(1000000), {
+    await client.cacheRootBanks(
+      mangoGroupPubkey,
+      mangoCache.publicKey,
+      rootBankPubkeys,
+      payer as unknown as Account
+    );
+
+    //await client.cachePrices
+
+    const tx = await program.rpc.deposit(new anchor.BN(1000000), 15, {
       accounts: {
         mangoProgram: MANGO_PROG_ID,
         pool: poolAddress,
@@ -121,12 +192,84 @@ describe("mango-blender", () => {
       signers: [payer],
     });
 
-    const providerQuoteAfter = await getTokenAccount(provider as unknown as SaberProvider, providerQuoteATA);
+    const providerQuoteAfter = await getTokenAccount(
+      provider,
+      providerQuoteATA
+    );
     assert.ok(providerQuoteAfter.amount.eq(new anchor.BN(4000000)));
 
-    const mangoAccount = await client.getMangoAccount(mangoAccountAddress, MANGO_PROG_ID);
+    const mangoAccount = await client.getMangoAccount(
+      mangoAccountAddress,
+      MANGO_PROG_ID
+    );
     assert.ok(mangoAccount.deposits[tokenIndex].toNumber() === 1);
+
+    const pool = await program.account.pool.fetch(poolAddress);
+    console.log(pool.totalUsdcDeposits);
+    assert.ok(pool.totalUsdcDeposits.toNumber() === 1000000);
   });
-
-
 });
+
+// it("allows a user to deposit TOKEN_A into the delegated mangoAccount", async () => {
+//   const providerABefore = await getTokenAccount(
+//     provider,
+//     providerAATA
+//   );
+//   assert.ok(providerABefore.amount.eq(new anchor.BN(5000000)));
+
+//   const group = await client.getMangoGroup(mangoGroupPubkey);
+//   const rootBanks = await group.loadRootBanks(provider.connection);
+//   const rootBankPubkeys = rootBanks
+//     .map((rb) => rb?.publicKey)
+//     .filter((pk) => pk !== undefined) as PublicKey[];
+//   const tokenIndex = group.getTokenIndex(tokenA.publicKey);
+//   const nodeBanks = await rootBanks[tokenIndex]?.loadNodeBanks(
+//     provider.connection
+//   );
+//   const mangoCache = await group.loadCache(provider.connection);
+//   if (!nodeBanks) {
+//     throw Error;
+//   }
+
+//   await client.cacheRootBanks(
+//     mangoGroupPubkey,
+//     mangoCache.publicKey,
+//     rootBankPubkeys,
+//     payer as unknown as Account
+//   );
+
+//   //await client.cachePrices
+
+//   const tx = await program.rpc.deposit(new anchor.BN(1000000), 15, {
+//     accounts: {
+//       mangoProgram: MANGO_PROG_ID,
+//       pool: poolAddress,
+//       mangoGroup: mangoGroupPubkey,
+//       mangoAccount: mangoAccountAddress,
+//       depositor: provider.wallet.publicKey,
+//       depositorTokenAccount: providerQuoteATA,
+//       mangoCache: mangoCache.publicKey,
+//       rootBank: rootBanks[tokenIndex]?.publicKey,
+//       nodeBank: nodeBanks[0].publicKey,
+//       vault: nodeBanks[0].vault,
+//       tokenProgram: TOKEN_PROGRAM_ID,
+//     },
+//     signers: [payer],
+//   });
+
+//   const providerQuoteAfter = await getTokenAccount(
+//     provider,
+//     providerQuoteATA
+//   );
+//   assert.ok(providerQuoteAfter.amount.eq(new anchor.BN(4000000)));
+
+//   const mangoAccount = await client.getMangoAccount(
+//     mangoAccountAddress,
+//     MANGO_PROG_ID
+//   );
+//   assert.ok(mangoAccount.deposits[tokenIndex].toNumber() === 1);
+
+//   const pool = await program.account.pool.fetch(poolAddress);
+//   console.log(pool.totalUsdcDeposits);
+//   assert.ok(pool.totalUsdcDeposits.toNumber() === 1000000);
+// });
--- tests/setupMango.ts
+++ tests/setupMango.ts
@@ -1,5 +1,8 @@
 import {
   Cluster,
+  Config,
+  getOracleBySymbol,
+  GroupConfig,
   MangoClient,
   zeroKey,
 } from "@blockworks-foundation/mango-client";
@@ -13,7 +16,7 @@ import {
 } from "@solana/web3.js";
 import { AccountLayout, Token, TOKEN_PROGRAM_ID, u64 } from "@solana/spl-token";
 import { createMintToInstruction, getOrCreateATA } from "@saberhq/token-utils";
-import { Provider as SaberProvider } from "@saberhq/token-utils/node_modules/@saberhq/solana-contrib";
+import * as fs from 'fs';
 
 // These params typically differ across currencies (and Spot vs Perp) based on risk
 // Since this is just for simple testing, it's ok to reuse them for everything
@@ -32,6 +35,8 @@ export const SERUM_PROG_ID = new PublicKey(
 export const MANGO_PROG_ID = new PublicKey(
   "mv3ekLzLbnVPNxjSKvqBpU3ZeZXPQdEC3bp5MDEBG68"
 );
+
+export const MANGO_CONFIG_PATH = './tests/fixtures/mangoConfig.json';
 
 export async function createMangoGroup(
   provider: SolanaProvider,
@@ -53,47 +58,78 @@ export async function createMangoGroup(
     payer as unknown as Account
   );
   console.log("Mango Group initialized");
+
+  const group = await client.getMangoGroup(groupPubkey);
+  const rootBanks = await group.loadRootBanks(provider.connection);
+  const tokenIndex = group.getTokenIndex(quoteToken.publicKey);
+  const nodeBanks = await rootBanks[tokenIndex]?.loadNodeBanks(provider.connection);
+
+  const tokenDesc = {
+    symbol: 'QUOTE',
+    mintKey: quoteToken.publicKey,
+    decimals: group.tokens[tokenIndex].decimals,
+    rootKey: rootBanks[tokenIndex]?.publicKey as PublicKey,
+    nodeKeys: nodeBanks?.map((n) => n?.publicKey) as PublicKey[],
+  };
+
+  const config = readConfig();
+  const groupDesc: GroupConfig = {
+    cluster: 'localnet',
+    name: 'localnet.1',
+    publicKey: groupPubkey,
+    quoteSymbol: 'QUOTE',
+    mangoProgramId: MANGO_PROG_ID,
+    serumProgramId: SERUM_PROG_ID,
+    tokens: [tokenDesc],
+    oracles: [],
+    perpMarkets: [],
+    spotMarkets: [],
+  };
+
+  config.storeGroup(groupDesc);
+  writeConfig(config);
   return groupPubkey;
 }
 
-export async function createQuoteToken(
+export async function createToken(
   provider: SolanaProvider,
-  payer: Keypair
+  payer: Keypair,
+  decimals: number
 ): Promise<Token> {
-  console.log("Creating quote token");
+  console.log("Creating token");
   return Token.createMint(
     provider.connection,
     payer as any,
     payer.publicKey,
     null,
-    6,
+    decimals,
     TOKEN_PROGRAM_ID
   );
 }
 
-export async function initializeProviderQuoteATA(provider: SolanaProvider, payer: Keypair, quoteToken: Token) {
+export async function initializeProviderATA(provider: SolanaProvider, payer: Keypair, token: Token, lots: number) {
     // Create associated token accounts
-    const createQuoteATAResult = await getOrCreateATA({
-      provider: provider as unknown as SaberProvider, // TODO: WTF
-      mint: quoteToken.publicKey,
+    const createATAResult = await getOrCreateATA({
+      provider: provider,
+      mint: token.publicKey,
     });
 
     // Mint tokens
-    const mintQuoteInstruction = createMintToInstruction({
-      provider: provider as unknown as SaberProvider,
-      mint: quoteToken.publicKey,
+    const mintInstruction = createMintToInstruction({
+      provider: provider,
+      mint: token.publicKey,
       mintAuthorityKP: payer,
-      to: createQuoteATAResult.address,
-      amount: new u64(5000000),
+      to: createATAResult.address,
+      amount: new u64(lots),
     });
 
     const transaction = new Transaction();
-    if (createQuoteATAResult.instruction) {
-      transaction.add(createQuoteATAResult.instruction);
+    if (createATAResult.instruction) {
+      transaction.add(createATAResult.instruction);
     }
-    transaction.add(...mintQuoteInstruction.instructions);
+    transaction.add(...mintInstruction.instructions);
     await provider.send(transaction);
-    return createQuoteATAResult.address
+    return createATAResult.address
 }
 
 async function initializeFeeVault(
@@ -133,3 +169,55 @@ async function initializeFeeVault(
   console.log("Fee vault initialized");
   return feeVaultKeypair.publicKey;
 }
+
+function readConfig(): Config {
+  return new Config(JSON.parse(fs.readFileSync(MANGO_CONFIG_PATH, 'utf-8')));
+}
+
+function writeConfig(config: Config): void {
+  fs.writeFileSync(MANGO_CONFIG_PATH, JSON.stringify(config.toJson(), null, 2));
+}
+
+
+export async function initPriceOracles(payer: Keypair, client: MangoClient, mangoGroupPubkey: PublicKey) : Promise<void> {
+  await Promise.all([
+    initPriceOracle(payer, client, mangoGroupPubkey, 'AAAA'),
+    initPriceOracle(payer, client, mangoGroupPubkey, 'BBBB'),
+  ])
+}
+
+
+export async function initPriceOracle(payer: Keypair, client: MangoClient, mangoGroupPubkey: PublicKey, symbol: string): Promise<void> {
+  await client.addStubOracle(
+    mangoGroupPubkey, payer as unknown as Account,
+  );
+  const group = await client.getMangoGroup(mangoGroupPubkey);
+    const config = readConfig();
+
+    const oracle = {
+      symbol,
+      publicKey: group.oracles[group.numOracles - 1],
+    };
+    const foundOracle = getOracleBySymbol(config.groups[0], symbol);
+    if (foundOracle) {
+      Object.assign(foundOracle, oracle);
+    } else {
+      config.groups[0].oracles.push(oracle);
+    }
+    console.log(`${symbol} price oracle added`);
+    writeConfig(config);
+}
+
+// async function setOraclePrice(payer: Keypair, client: MangoClient, mangoGroupPubkey: PublicKey, price: number) {
+//   const group = await client.getMangoGroup(mangoGroupPubkey);
+//   group.oracles
+
+  
+//   const groupConfig = config.groups[0];
+//   await client.setStubOracle(
+//     groupConfig.publicKey,
+//     oracle.publicKey,
+//     payer as unknown as Account,
+//     price,
+//   );
+// }
